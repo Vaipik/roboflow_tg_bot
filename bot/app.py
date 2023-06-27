797f44f2fcced6684c7d80bcca991d553d333793
Vaipik <n.khylko@gmail.com>
2023-06-27 10:49:58 +0300

    proper injection of roboflow

--- bot/app.py
+++ bot/app.py
@@ -2,18 +2,28 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 
+from bot.handlers import common_router, recognition_router
+from bot.utils import initialize_roboflow
 from config import cfg
 
 
 async def main():
     logging.basicConfig(
-        level=logging.WARNING,
-        format="%(asctime)s [%(levelname)s%] %(name)s - %(message)s"
+        level=logging.INFO,
+        # format="%(asctime)s [%(levelname)s%] - %(message)s"
     )
     bot = Bot(token=cfg.bot_token.get_secret_value(), parse_mode="HTML")
-    dp = Dispatcher()
-    await dp.start_polling(bot)
+
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(common_router)
+    dp.include_router(recognition_router)
+
+    roboflow_api = initialize_roboflow(cfg.roboflow)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, roboflow_api=roboflow_api)
 
 
 if __name__ == '__main__':
