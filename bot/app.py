9c4206a6c53aa63e23505cd6eb7b282f1d991323
Vaipik <n.khylko@gmail.com>
2023-06-29 15:36:29 +0300

    ui menu

--- bot/app.py
+++ bot/app.py
@@ -6,7 +6,9 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.handlers import common_router, recognition_router, response_router
 from bot.utils import initialize_roboflow
-from config import cfg
+from bot.ui_commands import bot_set_commands
+from bot.config import cfg
+
 
 logger = logging.getLogger(__name__)
 
@@ -18,8 +20,10 @@ async def main():
     dp.include_router(common_router)
     dp.include_router(recognition_router)
     dp.include_router(response_router)
+
     roboflow_api = initialize_roboflow(cfg.roboflow, cfg.bot_token.get_secret_value())
 
+    await bot_set_commands(bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, roboflow_api=roboflow_api)
 
---
+++ bot/ui_commands.py
@@ -0,0 +1,10 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats
+
+
+async def bot_set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Bot main menu"),
+        BotCommand(command="cancel", description="Stop any operation")
+    ]
+    await bot.set_my_commands(commands, scope=BotCommandScopeAllPrivateChats())
