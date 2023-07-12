72aaa6fdec17f254a71b860951db7992b8a8e04e
Vaipik <n.khylko@gmail.com>
2023-07-12 21:14:19 +0300

    extended dto.Response

--- bot/dto/response.py
+++ bot/dto/response.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass(frozen=True)
 class Response:
     """Dataclass to transferitn ORM response."""
 
+    uploaded_image_id: str
     recognized_image_id: str
+    generated_at: datetime
     objects: list["RecognizedObject"]  # type: ignore
+    model: "NeuralModel"  # type: ignore
 
     def get_labels(self) -> dict[str, int]:
         """Evaluate labels to dict with label: amount k-v."""
